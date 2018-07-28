739cec4c31fd495634d95ea0570532423566092d
sparskakyl <lykakspars@cock.li>
2018-07-28 19:28:10 +0000

    Boo Python!

---
+++ python/src/__init__.py
@@ -0,0 +1,9 @@
+# https://old.reddit.com/r/ProgrammerHumor/comments/92cuxg/how_to_lose_all_your_karma_in_one_easy_shitpost/
+boo = "AHHHH!!!"
+
+# <SOURCE NOT FOUND>
+def boo(booleanthing):
+	if booleanthing == True:
+		print(boo)
+	else:
+		print("...")
